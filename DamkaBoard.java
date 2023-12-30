f7cc92e84a77eaba564048fcc32da45d60013fa5
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2023-12-30 22:26:39 +0200

    DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,13 +1,14 @@
 public class DamkaBoard {
     public static void main(String[] args){
         int n = Integer.parseInt(args[0]);
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             if (i % 2 != 0){
                 System.out.print(" ");
             }
-            for (int j = 0; j < n; j++) {
-                System.out.print(" *");
+            for (int j = 0; j < n; j++){
+                System.out.print("* ");
             }
+                System.out.println();
                 System.out.println();
         }
     }
