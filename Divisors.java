9de3b9584609d1d46ce5756b13905c4098edd76d
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2023-12-30 19:09:52 +0200

    Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -1,8 +1,10 @@
-/**
- *  Gets a command-line argument (int), and prints all the divisors of the given number.
- */
 public class Divisors {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args){
+        int a = Integer.parseInt(args[0]);
+        for (int i = 1; i <= a; i++){
+           if (a % i == 0){
+            System.out.println(i);
+           }
+        }
+    }
 }
