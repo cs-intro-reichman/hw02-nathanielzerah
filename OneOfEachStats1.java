0d9753fe2b894c892616b8b62841d787a8364ef1
nathanielzerah <154419186+nathanielzerah@users.noreply.github.com>
2024-01-02 18:19:56 +0200

    OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -1,11 +1,46 @@
-/**
- *  Computes some statistics about families in which the parents decide 
- *  to have children until they have at least one child of each gender.
- *  The program expects to get one command-line argument: an int value
- * 	that determines how many families to simulate.
- */
-public class OneOfEachStats1 {
-	public static void main (String[] args) {
-		//// Put your code here
-	}
+public class OneOfEachStats1{
+    public static void main(String[] args){
+        int T = Integer.parseInt(args[0]);
+        int boy = 0;
+        int girl = 0;
+        boolean OneBoy;
+        boolean OneGirl;
+        int TwoChildren = 0;
+        int ThreeChildren = 0;
+        int MoreChildren = 0;
+        double TotalChildren = 0;
+        for (int x = 0; x < T; x++) {
+        do {
+            if (Math.random() < 0.5){
+                boy += 1;
+            } else {
+                girl += 1;
+            }
+            OneBoy = (boy >= 1);
+            OneGirl = (girl >= 1);
+        } while (!(OneBoy && OneGirl));
+        if ((boy + girl) == 2) {
+            TwoChildren += 1;
+        } else if ((boy + girl) == 3){
+            ThreeChildren += 1;
+        } else if ((boy + girl) >= 4){
+            MoreChildren += 1;
+        }
+        TotalChildren += (boy + girl);
+        boy = 0;
+        girl = 0;
+        }
+        System.out.println("Average: " + (TotalChildren / T) + " children to get at least one of each gender.");
+        int max =  Math.max(TwoChildren , Math.max(ThreeChildren , MoreChildren));
+        System.out.println("Number of families with 2 children: " + TwoChildren);
+        System.out.println("Number of families with 3 children: " + ThreeChildren);
+        System.out.println("Number of families with 4 or more children: " + MoreChildren);
+        if (max == TwoChildren) {
+            System.out.println("The most common number of children is 2.");
+        } else if (max == ThreeChildren) {
+            System.out.println("The most common number of children is 3.");
+        } else if (max == MoreChildren) {
+            System.out.println("The most common number of children is 4 or more.");
+        }
+    }
 }
